a9921261dc10cf33d9a2b3f9d420bbfbdfb1de9f
Benjamin GOUGUET <Benjamin@pc5.home>
2014-11-06 15:06:47 +0100

    Initial Commit

---
+++ jeuPendu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jeuPendu
+//
+//  Created by Benjamin GOUGUET on 06/11/2014.
+//  Copyright (c) 2014 Benjamin GOUGUET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
